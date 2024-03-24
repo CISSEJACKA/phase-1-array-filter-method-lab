14eaa5105f1a78cadd8490566710fd69dd2868e1
JACKA <jackarcisse@gmail.com>
2024-03-24 13:20:12 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+function findMatching(drivers , query) {
+    return drivers.filter(drivers => drivers.toLowerCase() === query.toLowerCase()) 
+
+}
+function fuzzyMatch(drivers, query) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(query.toLowerCase()));
+
+}
+// function matchName(drivers, name) {
+//     return drivers.filter(driver => drivername.toLowerCase() === name.toLowerCase());
+// }
+function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name.toLowerCase() === name.toLowerCase());
+}
+
